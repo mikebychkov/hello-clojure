46a7bead783966f1c7a4fbc88681f08eccdda841
Mike <mihailbichkov@yandex.ru>
2023-07-15 05:19:05 +0500

    init

---
+++ src/hello_clojure/ddz.clj
@@ -0,0 +1,37 @@
+(ns hello-clojure.ddz
+  (:require
+   [clojure.core]))
+
+(defn get-guess []
+  (println "Play your hand: (r)ock, (p)aper, (s)cissors")
+  (let [guess (read-line)]
+    (if (contains? #{"r" "p" "s"} guess) guess nil)))
+
+(defn winner [guess1 guess2]
+  (let [guesses [guess1 guess2]]
+    (cond
+      (= guess1 guess2) 0
+      (= guesses ["p" "r"]) 1
+      (= guesses ["r" "p"]) 2
+      (= guesses ["s" "r"]) 2
+      (= guesses ["r" "s"]) 1
+      (= guesses ["p" "s"]) 2
+      (= guesses ["s" "p"]) 1
+      )))
+
+(defn play-hand []
+  (let [computer-guess (rand-nth ["r" "p" "s"])
+        user-guess (get-guess)
+        winner (winner computer-guess user-guess)]
+    (println "The computer guess: " computer-guess)
+    (println "Your guess: " user-guess)
+    (cond
+      (= user-guess nil)(println "Your input was invalid.")
+      (= winner 0)(println "Game tied!")
+      (= winner 1)(println "Computer wins!")
+      (= winner 2)(println "You win!")
+      )))
+
+(loop []
+  (play-hand)
+  (recur))
---
+++ src/hello_clojure/trys.clj
@@ -0,0 +1,151 @@
+(ns hello-clojure.trys
+  (:require
+   [clojure.core]))
+
+
+(defn sayHi [name] 
+  (str "Hi, " name))
+
+(sayHi "Mike")
+
+(let [x (fn [x]
+         (sayHi x)
+         (let [x 7]
+           (sayHi x)))]
+  (x 3))
+
+;; 
+
+( seq [1 2 3 4]) 
+
+( peek [1 2 3 4]) 
+( pop [1 2 3 4]) 
+
+( first [1 2 3 4]) 
+( rest [1 2 3 4]) 
+
+;; 
+
+( seq '(1 2 3 4)) 
+
+( peek '(1 2 3 4)) 
+( pop '(1 2 3 4)) 
+
+( first '(1 2 3 4)) 
+( rest '(1 2 3 4)) 
+
+;; 
+
+( seq {1 2 3 4})
+
+( first {1 2 3 4})
+( rest {1 2 3 4})
+
+;; 
+
+(loop [x [1 2 3 4 5]]
+  (if (first x)
+    (do
+      (println (first x))
+      (recur (rest x)))))
+
+;; 
+
+(def z (cons 6 (cons 5 (cons 3 nil))))
+(first z)
+(rest z)
+(first (rest z))
+(rest (rest z))
+
+;; 
+
+(defn do-lazy [n b]
+  (lazy-seq
+   (if (> n b)
+     nil
+     (cons n (do-lazy (inc n) b)))))
+
+(do-lazy 1 10)
+(first (do-lazy 1 10))
+(first (rest (do-lazy 1 10)))
+
+(loop [l (do-lazy 1 10)]
+  (if (first l)
+    (do 
+      (println (first l))
+      (recur (rest l)))
+    (println "end")
+    ))
+
+;; 
+
+(defn zeroes []
+  (lazy-seq
+   (cons 0 (zeroes))))
+
+(first (zeroes))
+(first (rest (zeroes)))
+;; (print (rest (zeroes)))
+
+(loop [l (zeroes) n 10]
+  (if (> n 0)
+    (do 
+      (println (first l))
+      (recur (rest l) (dec n)))
+    (println "end")
+    ))
+
+;; 
+
+(repeatedly 3 rand)
+
+;; 
+
+(apply * (range 1 10))
+
+;; 
+
+(reduce + 20 [1 2 3 4])
+(reductions + 20 [1 2 3 4])
+
+;; 
+
+(import java.time.LocalDateTime)
+(print (LocalDateTime/now))
+
+;; 
+
+(keyword? :foo)
+(symbol? :foo)
+
+;; 
+
+(def my-set #{1 2 3})
+(print my-set)
+
+;; 
+
+(def m {:x 8 :y 89 :q 45})
+(let [{:keys [x q]} m]
+  (print x q))
+
+;; 
+
+([1 2 3] 1)
+({:x 13 :r 33} :r)
+(:x {:x 13 :r 33})
+
+;; 
+
+(def f-f #(print "hello" %2 %1))
+(f-f 33 44)
+
+;; 
+
+(def bar "QWAS")
+(def xx ^:foo ^bar [1 3 2])
+(meta xx)
+
+;; 
+
+(clojure.pprint/pprint (meta #'+))
